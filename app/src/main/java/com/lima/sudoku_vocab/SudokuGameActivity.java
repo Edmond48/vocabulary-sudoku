ba5e94ca30484ba283d66cf941a60ecb5ce8bb83
Edmond <hhn8@sfu.ca>
2023-02-03 16:08:10 -0800

    Clean and document SudokuGameActivity example
    Added comments and constants to make it easier to modify when model classes are ready.

--- app/src/main/java/com/lima/sudoku_vocab/SudokuGameActivity.java
+++ app/src/main/java/com/lima/sudoku_vocab/SudokuGameActivity.java
@@ -6,8 +6,6 @@ import android.annotation.SuppressLint;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.view.ViewTreeObserver;
 import android.widget.TableLayout;
 import android.widget.TableRow;
@@ -16,8 +14,8 @@ import android.widget.Toast;
 
 public class SudokuGameActivity extends AppCompatActivity {
 
-    // TODO for the whole file, replace hardcoded values
-    TextView[][] cells = new TextView[9][9];
+    private static final int BOARD_SIDE = 9;
+    private TextView[][] cells = new TextView[BOARD_SIDE][BOARD_SIDE];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,35 +25,40 @@ public class SudokuGameActivity extends AppCompatActivity {
         populateSudokuBoard();
     }
 
+    // TODO When model classes are ready, incorporate them here
+    // Create a 9x9 board and populate it with TextView
     private void populateSudokuBoard() {
         TableLayout table = findViewById(R.id.sudoku_table);
 
-        for (int i = 0; i < 9; i++) {
+        for (int i = 0; i < BOARD_SIDE; i++) {
             TableRow row = new TableRow(this);
             table.addView(row);
-            for (int j = 0; j < 9; j++) {
+            for (int j = 0; j < BOARD_SIDE; j++) {
                 final int rowIndex = i;
                 final int colIndex = j;
+
+                // TODO Refactor to a function
+                // Setup cell
                 TextView cell = new TextView(this);
                 cell.setText("Test");
                 cell.setTextAlignment(TextView.TEXT_ALIGNMENT_CENTER);
+
+                // Checker board pattern
                 if ((i+j)%2 == 0)
                     cell.setBackgroundColor(Color.YELLOW);
                 else
                     cell.setBackgroundColor(Color.BLUE);
 
-                cell.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        Toast.makeText(
-                                SudokuGameActivity.this,
-                                "You clicked the cell at (" + rowIndex + ", " + colIndex + ")",
-                                Toast.LENGTH_SHORT).show();
-                    }
-                });
+                // Toast the coordinate when a cell is clicked
+                cell.setOnClickListener(view -> Toast.makeText(
+                        SudokuGameActivity.this,
+                        "You clicked the cell at (" + rowIndex + ", " + colIndex + ")",
+                        Toast.LENGTH_SHORT).show());
 
+                // Set the cell's height to match its width after it has been drawn on the screen
                 cell.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
 
+                    // Handle correct method call on older Android versions
                     @SuppressLint("NewAPI")
                     @SuppressWarnings("deprecation")
                     @Override
@@ -64,10 +67,16 @@ public class SudokuGameActivity extends AppCompatActivity {
                             cell.getViewTreeObserver().removeGlobalOnLayoutListener(this);
                         else
                             cell.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+
+                        // make the cell square
                         cell.setHeight(cell.getWidth());
                     }
                 });
+
+                // Add the cell to the row
                 row.addView(cell);
+
+                // Add the cell to internal array
                 cells[i][j] = cell;
             }
         }
