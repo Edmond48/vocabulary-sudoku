69d1b63f600f5fc1ab391cc105aa5d281b619a8e
Edmond <hhn8@sfu.ca>
2023-02-07 17:37:09 -0800

    Work on basic UI
    Textview's display correctly and does not resize during runtime.
    
    Basic button actions implemented for User Story 4. Background of selected buttons are not highlighted yet.

--- app/src/main/java/com/lima/sudoku_vocab/SudokuGameActivity.java
+++ app/src/main/java/com/lima/sudoku_vocab/SudokuGameActivity.java
@@ -6,8 +6,10 @@ import android.annotation.SuppressLint;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.widget.TableLayout;
@@ -27,6 +29,9 @@ public class SudokuGameActivity extends AppCompatActivity {
     private final int BOARD_SIDE = board.getDimension();
     private TextView[][] cells = new TextView[BOARD_SIDE][BOARD_SIDE];
 
+    int selectedActionButtonIndex = BOARD_SIDE;
+    int selectedCellIndex = -1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +39,8 @@ public class SudokuGameActivity extends AppCompatActivity {
 
         populateSudokuBoard();
         populateWordButtons();
+
+        findViewById(R.id.wordDisplay).setBackgroundColor(Color.BLUE);
     }
 
     // TODO When model classes are ready, incorporate them here
@@ -42,7 +49,14 @@ public class SudokuGameActivity extends AppCompatActivity {
         TableLayout table = findViewById(R.id.sudoku_table);
 
         for (int i = 0; i < BOARD_SIDE; i++) {
+
             TableRow row = new TableRow(this);
+            row.setLayoutParams(new TableLayout.LayoutParams(
+                    TableLayout.LayoutParams.MATCH_PARENT,
+                    TableLayout.LayoutParams.MATCH_PARENT,
+                    1.0f
+            ));
+
             table.addView(row);
             for (int j = 0; j < BOARD_SIDE; j++) {
                 final int rowIndex = i;
@@ -51,23 +65,23 @@ public class SudokuGameActivity extends AppCompatActivity {
                 // TODO Refactor to a function
                 // Setup cell
                 TextView cell = new TextView(this);
-                cell.setText("Test");
-                cell.setTextAlignment(TextView.TEXT_ALIGNMENT_CENTER);
+                TableRow.LayoutParams params = new TableRow.LayoutParams(
+                        TableRow.LayoutParams.MATCH_PARENT,
+                        TableRow.LayoutParams.MATCH_PARENT,
+                        1.0f
+                );
+                params.setMargins(5, 5, 5, 5);
+                cell.setLayoutParams(params);
 
-                // Checker board pattern
-                if ((i+j)%2 == 0)
-                    cell.setBackgroundColor(Color.YELLOW);
-                else
-                    cell.setBackgroundColor(Color.BLUE);
+                cell.setTextAlignment(TextView.TEXT_ALIGNMENT_CENTER);
+                cell.setGravity(Gravity.CENTER);
+                cell.setBackgroundColor(Color.GRAY);
+                cell.setPadding(1, 1, 1, 1);
 
                 // Toast the coordinate when a cell is clicked
-                cell.setOnClickListener(view -> {
-                    Toast.makeText(
-                        SudokuGameActivity.this,
-                        "You clicked the cell at (" + rowIndex + ", " + colIndex + ")",
-                        Toast.LENGTH_SHORT).show();
-                    Log.i("GameActivity", "cell clicked");
-                });
+                cell.setOnClickListener(
+                        view -> onCellClick(view, rowIndex, colIndex)
+                );
 
                 // Set the cell's height to match its width after it has been drawn on the screen
                 makeTextViewSquare(cell);
@@ -81,47 +95,64 @@ public class SudokuGameActivity extends AppCompatActivity {
         }
     }
 
+    private void onCellClick(android.view.View v, int rowIndex, int colIndex) {
+        selectedCellIndex = rowIndex * BOARD_SIDE + colIndex;
+
+        if (selectedActionButtonIndex < BOARD_SIDE) {
+            board.setCell(rowIndex, colIndex, selectedActionButtonIndex);
+            cells[rowIndex][colIndex].setText(board.getWord(selectedActionButtonIndex).getNativeWord());
+        }
+    }
+
     private void populateWordButtons() {
         TableLayout wordButtons = findViewById(R.id.word_buttons);
         wordButtons.setBackgroundColor(Color.BLACK);
 
         for (int i = 0; i < Math.ceil(BOARD_SIDE / WORDS_PER_ROW); i++) {
             TableRow row = new TableRow(this);
-            wordButtons.addView(row);
 
             row.setLayoutParams(new TableLayout.LayoutParams(
                     TableLayout.LayoutParams.MATCH_PARENT,
                     TableLayout.LayoutParams.MATCH_PARENT,
                     1.0f
             ));
+            wordButtons.addView(row);
+
             for (int j = 0; j < WORDS_PER_ROW && j + i * WORDS_PER_ROW < BOARD_SIDE; j++) {
                 final int rowIndex = i;
                 final int colIndex = j;
 
                 TextView wordBtn = new TextView(this);
 
-                wordBtn.setText(board.getWord(colIndex + rowIndex * (int) WORDS_PER_ROW).getNativeWord());
-                wordBtn.setTextAlignment(TextView.TEXT_ALIGNMENT_CENTER);
-                wordBtn.setBackgroundColor(Color.GRAY);
-                wordBtn.setPadding(0, 10, 0, 10);
-
                 TableRow.LayoutParams params = new TableRow.LayoutParams(
-                        TableRow.LayoutParams.MATCH_PARENT,
+                        TableRow.LayoutParams.WRAP_CONTENT,
                         TableRow.LayoutParams.MATCH_PARENT,
                         1.0f
                 );
-                params.setMargins(10, 10, 10, 10);
+                params.setMargins(5, 5, 5, 5);
                 wordBtn.setLayoutParams(params);
 
-                wordBtn.setOnClickListener(view -> {
-                    Toast.makeText(
-                            this,
-                            "You press the word: " + board.getWord(colIndex + rowIndex * (int) WORDS_PER_ROW).getNativeWord(),
-                            Toast.LENGTH_SHORT).show();
-                });
+                wordBtn.setTextAlignment(TextView.TEXT_ALIGNMENT_CENTER);
+                wordBtn.setBackgroundColor(Color.GRAY);
+
+                final int wordIndex = j + i * (int) WORDS_PER_ROW;
+
+                wordBtn.setOnClickListener(
+                        view -> onActionButtonClick(view, wordIndex)
+                );
+
+                setUpWordButtonWhenDrawn(wordBtn, rowIndex, colIndex);
 
                 row.addView(wordBtn);
             }
+        }
+    }
+
+    private void onActionButtonClick(android.view.View v, int wordIndex) {
+        selectedActionButtonIndex = wordIndex;
+        if (selectedCellIndex > -1) {
+            cells[selectedCellIndex/BOARD_SIDE][selectedCellIndex%BOARD_SIDE].setText(board.getWord(selectedActionButtonIndex).getNativeWord());
+            selectedActionButtonIndex = BOARD_SIDE;
         }
     }
 
@@ -140,6 +171,41 @@ public class SudokuGameActivity extends AppCompatActivity {
 
                 // make the cell square
                 cell.setHeight(cell.getWidth());
+
+                cell.setMaxWidth(cell.getWidth());
+                cell.setMinimumWidth(cell.getWidth());
+
+                cell.setMaxHeight(cell.getHeight());
+                cell.setMinimumHeight(cell.getHeight());
+
+                cell.setBackgroundColor(Color.GRAY);
+                cell.setMaxLines(1);
+            }
+        });
+    }
+
+    private void setUpWordButtonWhenDrawn(TextView wordBtn, int rowIndex, int colIndex) {
+        wordBtn.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+
+            // Handle correct method call on older Android versions
+            @SuppressLint("NewAPI")
+            @SuppressWarnings("deprecation")
+            @Override
+            public void onGlobalLayout() {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN)
+                    wordBtn.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                else
+                    wordBtn.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+
+                // lock the width and the height
+                wordBtn.setMaxWidth(wordBtn.getWidth());
+                wordBtn.setMinimumWidth(wordBtn.getWidth());
+
+                wordBtn.setMaxHeight(wordBtn.getHeight());
+                wordBtn.setMinimumHeight(wordBtn.getHeight());
+
+                // set the text
+                wordBtn.setText(board.getWord(colIndex + rowIndex * (int) WORDS_PER_ROW).getNativeWord());
             }
         });
     }
