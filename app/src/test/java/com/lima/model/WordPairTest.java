da9ecd32b9f898700ec99834f284bd97314b988c
Aroofa Amin Panjwani <aap9@sfu.ca>
2023-02-21 15:19:21 -0800

    Added more tests for the WordPair class

--- app/src/test/java/com/lima/model/WordPairTest.java
+++ app/src/test/java/com/lima/model/WordPairTest.java
@@ -6,11 +6,25 @@ import org.junit.Test;
 
 public class WordPairTest {
 
+    private WordPair pair = new WordPair("car", "xe");
+
     // Tests if expected native and foreign words are returned for the word pair
     @Test
     public void testWordPairCreation() {
-        WordPair pair = new WordPair("car", "xe");
         assertEquals("car", pair.getNativeWord());
         assertEquals("xe", pair.getForeignWord());
     }
+
+    @Test
+    public void setNativeWord() {
+        pair.setNativeWord("home");
+        assertEquals("home", pair.getNativeWord());
+    }
+
+    @Test
+    public void setForeignWord() {
+        pair.setForeignWord("nha");
+        assertEquals("nha",pair.getForeignWord());
+    }
+
 }
